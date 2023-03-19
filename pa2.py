52ccda2f4f05cf303ff66f5c47499856bbbcb824
Tyler Kreider <tkreider@sandiego.edu>
2023-03-19 16:46:04 -0700

    inital file parse complete

--- pa2.py
+++ pa2.py
@@ -17,6 +17,42 @@ def pa2(filename):
     set of numbers.
     """
 
+
+    f = open(filename, 'r')
+    num_elements = int(f.readline())
+
+
+    
+
+    #set unordered list a
+    unordered_list_A = []
+    zero_index = f.readline().split()
+    first_index = f.readline().split()
+    i = 0
+    for item in zero_index:
+        unordered_list_A.append((int(item), int(first_index[i])))
+        i += 1
+
+    #set unordered list b
+    unordered_list_B = []
+    zero_index = f.readline().split()
+    first_index = f.readline().split()
+    i = 0
+    for item in zero_index:
+        unordered_list_B.append((int(item), int(first_index[i])))
+        i += 1
+
+    print("LIST A: ")
+    print(unordered_list_A)
+    print("LIST B: ")
+    print(unordered_list_B)
+
+
+    #start with a merge sort for bullet point one?
+
+    #bullet point two might need greedy alg
+
+
 if __name__ == "__main__":
     """
     Can run this file directly when testing your code.
