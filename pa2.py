6b9528371aac3382435a37be302edcbd27e609c7
Garrett <gcarney@sandiego.edu>
2023-03-24 16:16:39 -0700

    Finished?

--- pa2.py
+++ pa2.py
@@ -16,13 +16,8 @@ def pa2(filename):
     the order of the first set up numbers, and the order of the second
     set of numbers.
     """
-
-
     f = open(filename, 'r')
     num_elements = int(f.readline())
-
-
-    
 
     #set unordered list a
     unordered_list_A = getUnorderedList(f)
@@ -30,19 +25,7 @@ def pa2(filename):
     #set unordered list b
     unordered_list_B = getUnorderedList(f)
 
-    print("Original list A: ")
-    print(unordered_list_A)
-    print("Original list B: ")
-    print(unordered_list_B)
-
-    # sorted_A = merge_sort(unordered_list_A)
-    # sorted_B = merge_sort(unordered_list_B)
-    # print('\n'+"SORTED LIST A: ")
-    # print(sorted_A)
-    # print('\n'+"SORTED LIST B: ")
-    # print(sorted_B)
-
-    
+    #Create trees ordered by first element
     tree_a = RBTree(key = lambda x: x[0])
     for item in unordered_list_A:
         tree_a.insert(item)
@@ -51,138 +34,53 @@ def pa2(filename):
     for item in unordered_list_B:
         tree_b.insert(item)
 
-
-    #I put the trees into lists so it's more readable, idk if this is bad for later on
-    ordered_list_A = []
-    for i in range(len(tree_a)):
-        min = tree_a.delete_min()
-        ordered_list_A.append(min)
-    ordered_list_B = []
-    for i in range(len(tree_b)):
-        min = tree_b.delete_min()
-        ordered_list_B.append(min)
-
-
-    print("Ordered by first num list A:")
-    print(ordered_list_A)
-    print("Ordered by first num list B:")
-    print(ordered_list_B)
-
-
-
-    second_num_dict_A = {}
-    second_num_dict_B = {}
-    for item in unordered_list_A:
-        try:
-            second_num_dict_A[item[0]].insert(item)
-        except KeyError:
-            tree = RBTree(key = lambda item: item[1])
-            second_num_dict_A[item[0]] = tree
-            second_num_dict_A[item[0]].insert(item)
-
-
-    for item in unordered_list_B:
-        try:
-            second_num_dict_B[item[0]].insert(item)
-        except KeyError:
-            tree = RBTree(key = lambda item: item[1])
-            second_num_dict_B[item[0]] = tree
-            second_num_dict_B[item[0]].insert(item)
-
+    second_num_dict_A = createSecondNumDict(unordered_list_A)
+    second_num_dict_B = createSecondNumDict(unordered_list_B)
 
 
     final_list_A = []
     final_list_B = []
-   
-    #this is the body that i was trying to think throug, this isn't complete 
-    for i in range(len(ordered_list_A)):
+
+    for i in range(len(tree_a)):
         switch = False
-        #if theres a top/bottom conflict, then will enter if statement
-        if ordered_list_A[i][1] <= ordered_list_B[i][1]:
+        min_a = tree_a.delete_min()
+        min_b = tree_b.delete_min()
+        len_a_tree = len(second_num_dict_A[min_a[0]])
+        len_b_tree = len(second_num_dict_B[min_b[0]])
+
+        if(len_a_tree>=len_b_tree):
             try:
-                new_compare = second_num_dict_A[ordered_list_A[i][0]].delete_smallest_greater_than(ordered_list_B[i])
-                final_list_A.append(new_compare)
-                
+                new_b_node = second_num_dict_B[min_b[0]].delete_min()
+                new_compare_a_val = second_num_dict_A[min_a[0]].delete_smallest_greater_than(new_b_node)
+                final_list_A.append(new_compare_a_val)
+                final_list_B.append(new_b_node)
             except:
-                print("switching a doesn't work")
-                switch = True
-            
-            if switch == True:
-                try:
-                    new_compare = second_num_dict_B[ordered_list_B[i][0]].delete_largest_less_than(ordered_list_A[i])
-                    final_list_B.append(new_compare)
-                except:
-                    print("switching b doesn't work, no solution")
+                return None
         else:
-            final_list_A.append(ordered_list_A[i])
-            final_list_B.append(ordered_list_B[i])
+            try:
+                new_a_node = second_num_dict_A[min_a[0]].delete_min()
+                new_compare_b_val = second_num_dict_B[min_b[0]].delete_largest_less_than(new_a_node)
+                final_list_A.append(new_a_node)
+                final_list_B.append(new_compare_b_val)
+            except:
+                return None
 
-    print("final A")
-    print(final_list_A)
-    print("final B")
-    print(final_list_B)
+    final_output = getFinalOutput(final_list_A,final_list_B)
 
-
-
-           
-
-
+    return final_output
 
 
+def createSecondNumDict(unordered_list):
+    second_num_dict = {}
+    for item in unordered_list:
+        try:
+            second_num_dict[item[0]].insert(item)
+        except KeyError:
+            tree = RBTree(key = lambda item: item[1])
+            second_num_dict[item[0]] = tree
+            second_num_dict[item[0]].insert(item)
+    return second_num_dict
 
-
-
-    # print(second_num_dict_A[2].delete_min())
-    # print(second_num_dict_A[2].delete_min())
-    
-
-
-
-
-    
-    
-
-
-
-    #for item in unordered_list_A:
-
-
-
-
-    #start with a merge sort for bullet point one?
-
-    #bullet point two might need greedy alg
-
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-    left = merge_sort(left)
-    right = merge_sort(right)
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i][0] < right[j][0]:
-            result.append(left[i])
-            i += 1
-        elif left[i][0] == right[j][0]:
-            if left[i][1] < right[j][1]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
-        else:
-            result.append(right[j])
-            j += 1
-    result += left[i:]
-    result += right[j:]
-    return result
 
 def getUnorderedList(f):
     unordered_list = []
@@ -196,10 +94,20 @@ def getUnorderedList(f):
         i += 1
     return unordered_list
 
+def getFinalOutput(list_a,list_b):
+    final_indexes_a = []
+    final_indexes_b = []
+    for i in range(len(list_a)):
+        final_indexes_a.append(list_a[i][2])
+        final_indexes_b.append(list_b[i][2])
+    return (final_indexes_a,final_indexes_b)
+
+
 if __name__ == "__main__":
     """
     Can run this file directly when testing your code.
     """
-    ans = pa2("input1.txt")
+    ans = pa2("input8.txt")
+    print(ans)
 
 
